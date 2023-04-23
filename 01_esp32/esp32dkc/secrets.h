0c5cda8d9824074b6a10cdbc4b7783e12a63bd68
vorby01 <121613362+vorby01@users.noreply.github.com>
2023-04-23 14:19:46 +1000

    Create secrets.h

---
+++ 01_esp32/esp32dkc/secrets.h
@@ -0,0 +1,9 @@
+//wifi
+#define secret_wifi_ssid ""
+#define secret_wifi_password ""
+
+//mqtt
+#define secret_mqtt_ip ""
+#define secret_mqtt_port 1883
+#define secret_mqtt_username ""
+#define secret_mqtt_password ""
